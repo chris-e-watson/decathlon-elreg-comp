c3033780c5c921adac82165057a308db4fa5c24c
chris-e-watson <chris@watson.me.uk>
2016-07-13 17:34:45 +0100

    Added comment header block.

--- src/Decathlon/Decathlon/Decathlon.vb
+++ src/Decathlon/Decathlon/Decathlon.vb
@@ -1,4 +1,23 @@
-﻿Module Decathlon
+﻿'*******************************************************************************
+'* What:                                                                       *
+'*   Decathlon.vb                                                              *
+'* Who:                                                                        *
+'*   Chris Watson                                                              *
+'* When:                                                                       *
+'*   13 July 2016                                                              *
+'* Why:                                                                        *
+'*   Competition entry for The Register's Decathlon coding competition.        *
+'*   http://www.theregister.co.uk/2016/07/13/the_reg_coding_competition_10_tim *
+'*   es_as_hard_as_the_last_one/                                               *
+'* Remarks:                                                                    *
+'*   The rules (http://www.theregister.co.uk/Design/page/hub/ibm2016/#four)    *
+'*   stipulate that "You must submit the solution for each question as a       *
+'*   single source file called Decathlon.<e>, where <e> must be replaced with  *
+'*   the appropriate extension for the language used." hence the one large     *
+'*   file.                                                                     *
+'*******************************************************************************
+
+Module Decathlon
 
     Sub Main()
 
