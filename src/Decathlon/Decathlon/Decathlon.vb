f24512c834a54eef09e9360a9a528cbc2a1f6da9
chris-e-watson <chris@watson.me.uk>
2016-07-14 17:54:50 +0100

    Added some classes and enumerations related to reading an input file.

--- src/Decathlon/Decathlon/Decathlon.vb
+++ src/Decathlon/Decathlon/Decathlon.vb
@@ -17,6 +17,182 @@
 '*   file.                                                                     *
 '*******************************************************************************
 
+#Region "Enumerations"
+
+''' <summary>
+''' Enumeration of the different event types.
+''' </summary>
+Friend Enum EventType
+    
+    ''' <summary>
+    ''' The event type is unknown or not specified.
+    ''' </summary>
+    None                         = 0
+        
+    ''' <summary>
+    ''' The 100m sprint event.
+    ''' </summary>
+    OneHundredMetreSprint        = 1
+        
+    ''' <summary>
+    ''' The 110m hurdles event.
+    ''' </summary>
+    OneHundredAndTenMetreHurdles = 2
+        
+    ''' <summary>
+    ''' The 400m sprint event.
+    ''' </summary>
+    FourHundredMetreSprint       = 3
+        
+    ''' <summary>
+    ''' The 1500m sprint event.
+    ''' </summary>
+    FifteenHundredMetreSprint    = 4
+        
+    ''' <summary>
+    ''' The discus event.
+    ''' </summary>
+    Discus                       = 5
+        
+    ''' <summary>
+    ''' The javelin event.
+    ''' </summary>
+    Javelin                      = 6
+        
+    ''' <summary>
+    ''' The shot put event.
+    ''' </summary>
+    ShotPut                      = 7
+        
+    ''' <summary>
+    ''' The long jump event.
+    ''' </summary>
+    LongJump                     = 8
+    
+    ''' <summary>
+    ''' The high jump event.
+    ''' </summary>
+    HughJump                     = 9
+        
+    ''' <summary>
+    ''' The pole vault event.
+    ''' </summary>
+    PoleVault                    = 10
+End Enum
+
+#End Region
+
+#Region "Classes"
+
+''' <summary>
+''' Represents a single input data item.
+''' </summary>
+Friend Class InputDataItem
+
+#Region "Internal Properties"
+
+    ''' <summary>
+    ''' Gets or sets the name of the entrant.
+    ''' </summary>
+    Friend Property EntrantName As String
+    
+
+    ''' <summary>
+    ''' Gets or sets the type of the event.
+    ''' </summary>
+    Friend Property EventType As EventType
+
+
+    ''' <summary>
+    ''' Gets or sets the measurement for the named entrant's performance in the
+    ''' specified event.
+    ''' </summary>
+    Friend Property Measurement As Decimal
+    
+#End Region
+
+#Region "Internal Constructors"
+
+    ''' <summary>
+    ''' Initialises a new instance of the <see cref="InputDataItem"/> class.
+    ''' </summary>
+    Friend Sub New()
+    End Sub
+
+
+    ''' <summary>
+    ''' Initialises a new instance of the <see cref="InputDataItem"/> class.
+    ''' </summary>
+    ''' <param name="entrantName">
+    ''' The name of the entrant.
+    ''' </param>
+    ''' <param name="eventType">
+    ''' The type of the event.
+    ''' </param>
+    ''' <param name="measurement">
+    ''' The measurement.
+    ''' </param>
+    Friend Sub New(ByVal entrantName As String, ByVal eventType As EventType,
+                   ByVal measurement As Decimal)
+
+        Me.EntrantName = entrantName
+        Me.EventType   = eventType
+        Me.Measurement = measurement
+
+    End Sub
+
+#End Region
+
+End Class
+
+
+''' <summary>
+''' Represents a single input data set. An input data set contains information
+''' about event measurements for entrants for a single decathlon.
+''' </summary>
+Friend Class InputDataSet
+
+#Region "Private Fields"
+        
+    ''' <summary>
+    ''' The collection of input data items.
+    ''' </summary>
+    Private _items As List(Of InputDataItem) = New List(Of InputDataItem)
+
+#End Region
+
+#Region "Internal Properties"
+    
+    ''' <summary>
+    ''' Gets the items.
+    ''' </summary>
+    ''' <value>
+    ''' The items.
+    ''' </value>
+    Friend ReadOnly Property Items As List(Of InputDataItem)
+        Get
+            Return _items
+        End Get
+    End Property
+
+#End Region
+
+#Region "Internal Constructors"
+
+    ''' <summary>
+    ''' Initialises a new instance of the <see cref="InputDataSet"/> class.
+    ''' </summary>
+    Friend Sub New()
+    End Sub
+
+#End Region
+
+End Class
+
+#End Region
+
+#Region "Modules"
+
 ''' <summary>
 ''' The main application class.
 ''' </summary>
@@ -33,3 +209,5 @@ Module Decathlon
 #End Region
 
 End Module
+
+#End Region
