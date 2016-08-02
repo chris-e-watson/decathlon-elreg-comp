faef9c910f47e5fe153c2b54d2a888ecc6e5ec71
chris-e-watson <chris@watson.me.uk>
2016-08-02 18:35:39 +0100

    Added CalculatePoints_DiscusWorldRecordScore_Returns1383Points() unit test method.

--- src/Decathlon/Decathlon.Tests/PointsCalculatorTest.WorldRecords.vb
+++ src/Decathlon/Decathlon.Tests/PointsCalculatorTest.WorldRecords.vb
@@ -1,6 +1,30 @@
 ﻿Public Class PointsCalculatorTest
 
     <TestMethod()> _
+    Public Sub CalculatePoints_DiscusWorldRecordScore_Returns1383Points()
+        
+        ' Arrange.
+        '
+        Dim eventType As EventType = EventType.Discus
+        Dim score As Decimal = CDec(74.08)
+        Dim target As PointsCalculator = New PointsCalculator(eventType, score)
+        Dim expected As Long = 1383
+
+        ' 12.91 * ((74.08 - 4) ^ 1.1) = 1,383.8205...
+        
+        ' Act.
+        '
+        target.CalculatePoints()
+        Dim actual As Long = target.Points
+
+        ' Assert.
+        '
+        Assert.AreEqual(expected, actual)
+
+    End Sub
+
+
+    <TestMethod()> _
     Public Sub CalculatePoints_OneHundredMetreSprintWorldRecordScore_Returns1200Points()
         
         ' Arrange.
@@ -9,6 +33,8 @@
         Dim score As Decimal = CDec(9.59)
         Dim target As PointsCalculator = New PointsCalculator(eventType, score)
         Dim expected As Long = 1200
+
+        ' 25.4347 * ((18 - 9.59) ^ 1.81) = 1200.3485...
         
         ' Act.
         '
