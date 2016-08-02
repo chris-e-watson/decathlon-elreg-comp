6fe93f644cb1e59b4b07d6cdb195b264aa300da6
chris-e-watson <chris@watson.me.uk>
2016-08-02 14:34:07 +0100

    Added CalculatePoints_OneHundredMetreSprintScoreIsBelowBConstantValue_ZeroPoints() test. Currently failing.

---
+++ src/Decathlon/Decathlon.Tests/PointsCalculatorTest.vb
@@ -0,0 +1,80 @@
+﻿Imports System
+
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+Imports Decathlon
+
+
+
+'''<summary>
+'''This is a test class for PointsCalculatorTest and is intended
+'''to contain all PointsCalculatorTest Unit Tests
+'''</summary>
+<TestClass()> _
+Public Class PointsCalculatorTest
+
+
+    Private testContextInstance As TestContext
+
+    '''<summary>
+    '''Gets or sets the test context which provides
+    '''information about and functionality for the current test run.
+    '''</summary>
+    Public Property TestContext() As TestContext
+        Get
+            Return testContextInstance
+        End Get
+        Set(value As TestContext)
+            testContextInstance = Value
+        End Set
+    End Property
+
+#Region "Additional test attributes"
+    '
+    'You can use the following additional attributes as you write your tests:
+    '
+    'Use ClassInitialize to run code before running the first test in the class
+    '<ClassInitialize()>  _
+    'Public Shared Sub MyClassInitialize(ByVal testContext As TestContext)
+    'End Sub
+    '
+    'Use ClassCleanup to run code after all tests in a class have run
+    '<ClassCleanup()>  _
+    'Public Shared Sub MyClassCleanup()
+    'End Sub
+    '
+    'Use TestInitialize to run code before running each test
+    '<TestInitialize()>  _
+    'Public Sub MyTestInitialize()
+    'End Sub
+    '
+    'Use TestCleanup to run code after each test has run
+    '<TestCleanup()>  _
+    'Public Sub MyTestCleanup()
+    'End Sub
+    '
+#End Region
+
+
+    <TestMethod()> _
+    Public Sub CalculatePoints_OneHundredMetreSprintScoreIsBelowBConstantValue_ZeroPoints()
+        
+        ' Arrange.
+        '
+        Dim eventType As EventType = EventType.OneHundredMetreSprint
+        Dim score As Decimal = CDec(18.1)
+        Dim target As PointsCalculator = New PointsCalculator(eventType, score)
+        Dim expected As Long = 0
+        
+        ' Act.
+        '
+        Dim actual As Long = target.Points
+        target.CalculatePoints()
+
+        ' Assert.
+        '
+        Assert.AreEqual(expected, actual)
+
+    End Sub
+
+End Class
